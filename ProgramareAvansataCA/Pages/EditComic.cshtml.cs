a54e9edb825bf9952ca9cd3d78c47ed2a85f94d3
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2019-10-19 19:40:26 +0300

    EditComic

--- ProgramareAvansataCA/Pages/EditComic.cshtml.cs
+++ ProgramareAvansataCA/Pages/EditComic.cshtml.cs
@@ -13,12 +13,8 @@ namespace ProgramareAvansataCA.Pages
     public class EditComicModel : PageModel
     {
         private readonly ComicBookController _comicsController;
-
         [BindProperty]
-        public List<ComicBookViewModel> ComicBooks { get; set; }
-
-        [BindProperty]
-        public ComicBookViewModel SingleComicBook { get; set; }
+        public ComicBookViewModel ComicBook { get; set; }
 
         [BindProperty]
         public bool IsById { get; set; }
@@ -27,6 +23,18 @@ namespace ProgramareAvansataCA.Pages
         {
             _comicsController = new ComicBookController(ctx);
         }
+        public async Task<IActionResult> OnGet(int id)
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToPage("/Login");
 
+           ComicBook = await _comicsController.GetByIdAsync(id);
+            return Page();
+        }
+        public async Task<IActionResult> OnPostEdit()
+        {
+            await _comicsController.Edit(ComicBook);
+            return RedirectToPage("/Comics");
+        }
     }
 }
